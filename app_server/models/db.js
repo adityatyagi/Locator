2e4b55d0aa6bf9a4d8c3235da90d7377a9498b1b
Aditya Tyagi <tyagi.aditya844747@gmail.com>
2017-11-17 13:51:28 +0530

    Database Connection completed

---
+++ app_server/models/db.js
@@ -0,0 +1,76 @@
+require('constants').SIGTERM;
+require('constants').SIGUSR2;
+require('path').win32;
+
+var mongoose = require('mongoose');
+
+// Locator is the name of the database with which we will work
+var dbURI = 'mongodb://localhost:27017/Locator';
+
+// using named connection, this is used when we are working with multiple dbs
+// var locatorDB = mongoose.createConnection(dbURI);
+
+mongoose.connect(dbURI);
+
+// to emit the SIGINT event in windows so that the node application can hear the disconnection event
+var readLine = require('readline');
+
+if (process.platform === "win32") {
+    var rl = readLine.createInterface({
+        input: process.stdin,
+        outout: process.stdout
+    });
+
+    rl.on("SIGINT", function() {
+        process.emit("SIGINT");
+    });
+}
+
+
+
+// Mongoose will publish the events based on the status of the connection
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error ' + err);
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+
+// Closing the database is a asynchronous activity
+var gracefullShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+// When using nodemon to run the application, nodemon listens to SIGUSR2 event
+// For nodemon restarts
+// https://forums.manning.com/posts/list/34823.page
+// https://github.com/remy/nodemon/issues/140
+// this will not work.
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+// event handler for the event SIGINT event emitted
+process.on('SIGINT', function() {
+    gracefullShutdown('app termination with Ctrl+C', function() {
+        process.exit(0);
+    });
+});
+
+// app shutdown in HEROKU
+process.on('SIGTERM', function() {
+    console.log('Heroku app shoutdown', function() {
+        process.exit(0);
+    });
+});
